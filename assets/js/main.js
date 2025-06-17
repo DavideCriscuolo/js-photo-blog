c0f3e7584cc02dd94db77b30a80b91608945511f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-17 10:43:18 +0200

    Add initial JavaScript file and HTML structure for API integration
    - Created main.js to handle AJAX calls to the provided API endpoint.
    - Added comments outlining the project milestones and font requirements.
    - Initialized index.html for future content and structure.

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*CONSEGNA
+Milestone 2
+Utilizzando Postman, testiamo una chiamata a questo endpoint:
+https://lanciweb.github.io/demo/api/pictures/
+Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.
+
+Milestone 3
+Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+Font utilizzati:
+titoli: ‘Edu Tas Beginner’, sans-serif;d ate: ‘Sometype Mono’, ‘monospace’; (Dovreste sapere a questo punto cosa e come prendere da Google Fonts… :occhiolino:)
+
+*/

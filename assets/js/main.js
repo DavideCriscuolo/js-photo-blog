c0598c236b4366d099f7595353d8a2f437e8b790
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-19 10:35:17 +0200

    riprodotto blog con dom api

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,22 +19,76 @@ fetch(endpoint)
     const rowEl = document.querySelector(".row");
     data.forEach((singolCard) => {
       const { title, date, url } = singolCard;
-      const divColEl = document.createElement("div");
-      divColEl.classList.add("col");
-      divColEl.innerHTML = `
-          <div class="container_pin d-flex-center"><img class="pin" src="./assets/img/pin.svg" alt=""> </div>
-        <div class="card">
-         <div id="cardUp" class="card_up">
-           <img  src="${url}" alt="">
-            </div>
-            <div class="card_down">
-                <p>${title}</p>
-                <span>${date}</span>
-            </div>
-        </div> `;
-      rowEl.appendChild(divColEl);
-      const cardEl = divColEl.querySelector(".card");
-      const img = cardEl.querySelector("img");
+
+      // Con tamplate litteral
+
+      // const divColEl = document.createElement("div");
+      // divColEl.classList.add("col");
+      // divColEl.innerHTML = `
+      //     <div class="container_pin d-flex-center"><img class="pin" src="./assets/img/pin.svg" alt=""> </div>
+      //   <div class="card">
+      //    <div id="cardUp" class="card_up">
+      //      <img  src="${url}" alt="">
+      //       </div>
+      //       <div class="card_down">
+      //           <p>${title}</p>
+      //           <span>${date}</span>
+      //       </div>
+      //   </div> `;
+      // rowEl.appendChild(divColEl);
+      // const cardEl = divColEl.querySelector(".card");
+      // const img = cardEl.querySelector("img");
+      // img.addEventListener("click", function (e) {
+      //   e.preventDefault();
+      //   const overlayEl = document.querySelector(".overlay");
+      //   overlayEl.classList.remove("d-none");
+      //   overlayEl.classList.add("d-flex-center");
+      //   const imgEl = document.getElementById("imgModal");
+      //   imgEl.src = url;
+
+      //   const buttonClose = document.getElementById("closeModal");
+      //   buttonClose.addEventListener("click", function () {
+      //     overlayEl.classList.remove("d-flex-center");
+      //     overlayEl.classList.add("d-none");
+      //   });
+      // });
+
+      //Con DOM Api
+
+      //devo creare una col-containerPin->img  -card-carUp>img-carDown>title>data
+
+      const colEl = document.createElement("div");
+      colEl.classList.add("col");
+
+      const containerPin = document.createElement("div");
+      containerPin.classList.add("d-flex-center", "container_pin");
+      const imgPin = document.createElement("img");
+      imgPin.classList.add("pin");
+      imgPin.src = "./assets/img/pin.svg";
+      const cardElL = document.createElement("div");
+      cardElL.classList.add("card");
+      const cardUPEl = document.createElement("div");
+      cardUPEl.classList.add("card_up");
+      const imgPhoto = document.createElement("img");
+      imgPhoto.src = url;
+      const cardDown = document.createElement("div");
+      cardDown.classList.add("card_down");
+      const titlePhoto = document.createElement("p");
+      titlePhoto.innerHTML = title;
+      const dateEl = document.createElement("span");
+      dateEl.innerHTML = date;
+
+      colEl.appendChild(containerPin);
+      containerPin.appendChild(imgPin);
+      colEl.appendChild(cardElL);
+      cardElL.appendChild(cardUPEl);
+      cardUPEl.appendChild(imgPhoto);
+      cardElL.appendChild(cardDown);
+      cardDown.appendChild(titlePhoto);
+      cardDown.appendChild(dateEl);
+      rowEl.appendChild(colEl);
+
+      const img = cardElL.querySelector("img");
       img.addEventListener("click", function (e) {
         e.preventDefault();
         const overlayEl = document.querySelector(".overlay");
@@ -50,15 +104,15 @@ fetch(endpoint)
         });
       });
 
-      const containerPinEl = divColEl.querySelector(".pin");
+      const containerPinEl = colEl.querySelector(".pin");
 
-      cardEl.addEventListener("mouseenter", function () {
-        cardEl.classList.add("hover");
+      cardElL.addEventListener("mouseenter", function () {
+        cardElL.classList.add("hover");
         containerPinEl.classList.remove("d-flex-center");
         containerPinEl.classList.add("d-none");
       });
-      cardEl.addEventListener("mouseleave", function () {
-        cardEl.classList.remove("hover");
+      cardElL.addEventListener("mouseleave", function () {
+        cardElL.classList.remove("hover");
         containerPinEl.classList.remove("d-none");
         containerPinEl.classList.add("d-flex-center");
       });
